639644ff4accb36edf21581c4f6a0adac334437f
stfDeveloper <sstefancosmin@icloud.com>
2022-03-07 18:22:00 +0100

    first commit

---
+++ database/migrations/2022_03_07_161351_create_travels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('luogo', 50);
+            $table->string('informazioni', 100);
+            $table->string('partenza', 10);
+            $table->integer('durata', 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
---
+++ database/seeds/Travels.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Travel;
+
+class Travels extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run (Faker $faker)
+    {
+        for ($i=0; $i <20 ; $i++) { 
+            $travel = new Travel();
+            $travel->luogo =$faker->word(1); 
+            $travel->informazioni = $faker->text(100); 
+            $travel->partenza =$faker->dayOfWeek(); 
+            $travel->durata = $faker->randomDigit(); 
+            $travel->save();
+        }
+    }
+}
